6844e17a1f45765fef17cac76fa00c3dba855afc
Davis <daviskipkiruitoo@gmail.com>
2021-07-09 10:23:35 +0300

    fixed player error

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -46,8 +46,8 @@ $tr = $torrents;
                                         class="flex justify-start cursor-pointer text-white-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
                                         <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
                                         <div class="flex-grow font-medium px-2">{{$torrent['title']}}</div>
-                                        <div class="text-sm font-normal text-gray-500 tracking-wide">{{$torrent['name']}}
-                                        </div>
+                                        {{-- <div class="text-sm font-normal text-gray-500 tracking-wide">{{$torrent['name']}} --}}
+                                        {{-- </div> --}}
                                     </div>
                                 </a>
                             @else
