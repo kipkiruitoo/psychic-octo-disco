c3741c25795c1d3d4af79c958624fe2c157ee8dd
Davis <daviskipkiruitoo@gmail.com>
2021-07-21 11:19:17 +0300

    fixed releasedate issue

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-8">
     <a href="{{ route('movies.show', $movie['id']) }}">
-        <img src="{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150">
+        <img src="{{ $movie['poster_path'] }}" alt="{{ $movie['title'] }}" class="hover:opacity-75 transition ease-in-out duration-150">
     </a>
     <div class="mt-2">
         <a href="{{ route('movies.show', $movie['id']) }}" class="text-lg mt-2 hover:text-gray-300">{{ $movie['title'] }}</a>
